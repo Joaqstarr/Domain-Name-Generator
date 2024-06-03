8b906500395fd084601fdac833eae5062b843d18
Joaquin Royer <73140474+Joaqstarr@users.noreply.github.com>
2024-06-03 23:50:49 +0000

    array created and displayed on webpage

--- src/app.js
+++ src/app.js
@@ -8,4 +8,37 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  const allDomains = GetAllNames();
+
+  var ul = document.createElement("ul");
+
+  document.getElementById("renderList").appendChild(ul);
+
+  for (const domain of allDomains) {
+    var li = document.createElement("li");
+
+    li.innerText = domain;
+    ul.appendChild(li);
+  }
+  console.log(allDomains);
 };
+
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let extensions = [".com", ".us", ".org"];
+
+function GetAllNames() {
+  const domains = [];
+  for (const pnoun of pronoun) {
+    for (const adjetive of adj) {
+      for (const n of noun) {
+        for (const extension of extensions) {
+          let tempName = pnoun + adjetive + n + extension;
+          domains.push(tempName);
+        }
+      }
+    }
+  }
+  return domains;
+}
